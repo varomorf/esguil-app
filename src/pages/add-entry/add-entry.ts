32667e69fcdefdb2bc4d3dd0256aa259ea0fbc7b
Alfergon <alvaro@blunder.es>
2017-12-29 11:31:59 +0100

    Minimal entry creation

--- src/pages/add-entry/add-entry.ts
+++ src/pages/add-entry/add-entry.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {AlertController, NavController} from 'ionic-angular';
+import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
+import {JournalEntry} from "../../model/journalEntry";
 
 @Component({
   selector: 'page-add-entry',
@@ -7,8 +9,51 @@ import { NavController } from 'ionic-angular';
 })
 export class AddEntryPage {
 
-  constructor(public navCtrl: NavController) {
+  entriesRef: AngularFireList<JournalEntry>;
 
+  constructor(public navCtrl: NavController,
+              public alertCtrl: AlertController,
+              public db: AngularFireDatabase) {
+    this.entriesRef = db.list('entries');
+  }
+
+  createTask() {
+    let newEntryModal = this.alertCtrl.create({
+      title: 'New Entry',
+      message: "Enter a concept and amount for your new entry",
+      inputs: [
+        {
+          name: 'concept',
+          placeholder: 'Concept'
+        },
+        {
+          name: 'amount',
+          placeholder: 'Amount',
+          type: 'number'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.entriesRef.push({
+              date: new Date(),
+              concept: data.concept,
+              amount: data.amount,
+              payers: [],
+              targets: []
+            });
+          }
+        }
+      ]
+    });
+    newEntryModal.present(newEntryModal);
   }
 
 }
