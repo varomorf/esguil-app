45126b2b2dcc5c885a6e52b52bc450bff72bd0a3
Alfergon <alvaro@blunder.es>
2018-01-13 22:05:25 +0100

    Test group by month of year

--- src/pages/journal/journal.ts
+++ src/pages/journal/journal.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
-import {Observable} from "rxjs/Observable";
 import {JournalEntry} from "../../model/journalEntry";
 import {EntryProvider} from "../../providers/entries/EntryProvider";
 
@@ -9,15 +8,46 @@ import {EntryProvider} from "../../providers/entries/EntryProvider";
   templateUrl: 'journal.html'
 })
 export class JournalPage {
-  entries: Observable<JournalEntry[]>;
+  groupedEntries: Array<GroupedEntries> = [];
 
   constructor(public navCtrl: NavController,
               private entryProvider: EntryProvider) {
-    this.entries = this.entryProvider.entriesRef.valueChanges();
+    this.entryProvider.entriesRef.valueChanges()
+      .subscribe(entries => {
+        console.log(entries);
+
+        entries.forEach(entry => {
+          let date = new Date(entry.date);
+          let month = date.getMonth() + 1;
+          let year = date.getFullYear();
+          let key = month + '-' + year;
+
+          let i = this.groupedEntries.findIndex(g => g.key === key);
+          let grouped: GroupedEntries;
+          if (i === -1) {
+            grouped = new GroupedEntries();
+            grouped.key = key;
+
+            this.groupedEntries.push(grouped);
+          } else {
+            grouped = this.groupedEntries[i];
+          }
+
+          grouped.entries.push(entry);
+        });
+      });
+
   }
 
   removeEntry() {
 
   }
 
+
+}
+
+class GroupedEntries {
+  key: string;
+  year: string;
+  entries: Array<JournalEntry> = [];
 }
