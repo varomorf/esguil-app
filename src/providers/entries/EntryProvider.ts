be7122194bde25e1bd6a0c0245c2a9225fdcf402
Alfergon <alvaro@blunder.es>
2018-01-18 20:43:36 +0100

    Format

--- src/providers/entries/EntryProvider.ts
+++ src/providers/entries/EntryProvider.ts
@@ -1,7 +1,7 @@
 ///<reference path="../../../node_modules/angularfire2/database/interfaces.d.ts"/>
 import {Injectable} from "@angular/core";
 import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
-import {JournalEntry} from "../../model/journalEntry";
+import {GroupedEntries, JournalEntry} from "../../model/journalEntry";
 import * as firebase from "firebase";
 import {CurrentUserProvider} from "../users/CurrentUserProvider";
 import ThenableReference = firebase.database.ThenableReference;
@@ -9,22 +9,22 @@ import ThenableReference = firebase.database.ThenableReference;
 @Injectable()
 export class EntryProvider {
 
-  public entriesRef: AngularFireList<JournalEntry>;
+	public entriesRef: AngularFireList<JournalEntry>;
 
-  constructor(private db: AngularFireDatabase,
-              private currentUser: CurrentUserProvider) {
-    this.currentUser.currentUserObservable.subscribe(currentUser =>
-      this.entriesRef = db.list('entries', ref => ref.orderByChild('groupId').equalTo(currentUser.groupId))
-    );
-  }
+	constructor(private db: AngularFireDatabase,
+				private currentUser: CurrentUserProvider) {
+		this.currentUser.currentUserObservable.subscribe(currentUser =>
+			this.entriesRef = db.list('entries', ref => ref.orderByChild('groupId').equalTo(currentUser.groupId))
+		);
+	}
 
-  create(entry: JournalEntry): ThenableReference {
-    let entryData = {};
-    Object.assign(entryData, entry);
+	create(entry: JournalEntry): ThenableReference {
+		let entryData = {};
+		Object.assign(entryData, entry);
 
-    entryData['date'] = entry.date.toISOString();
+		entryData['date'] = entry.date.toISOString();
 
-    return this.entriesRef.push(entryData as JournalEntry);
-  }
+		return this.entriesRef.push(entryData as JournalEntry);
+	}
 
 }
