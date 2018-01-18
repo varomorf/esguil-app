d6722cee993b648a05cdb48484b0d0eebe98523c
Alfergon <alvaro@blunder.es>
2018-01-18 19:57:57 +0100

    Add factory method from other objects

--- src/model/journalEntry.ts
+++ src/model/journalEntry.ts
@@ -1,12 +1,28 @@
-export class JournalEntry {
-  groupId: string;
-  date: Date;
-  concept: string;
-  amount: number;
-  payers: string[];
-  targets: string[];
+import * as moment from 'moment';
 
-  constructor () {
-    this.date = new Date();
-  }
+export class JournalEntry {
+	groupId: string;
+	date: Date;
+	concept: string;
+	amount: number;
+	payers: string[];
+	targets: string[];
+
+	constructor() {
+		this.date = new Date();
+	}
+
+	static fromObject(data: any): JournalEntry {
+		let journalEntry = new JournalEntry();
+
+		journalEntry.groupId = data.groupId;
+		journalEntry.date = moment(data.date).toDate();
+		journalEntry.concept = data.concept;
+		journalEntry.amount = Number(data.amount);
+		journalEntry.payers = data.payers;
+		journalEntry.targets = data.targets;
+
+		return journalEntry;
+	}
+
 }
