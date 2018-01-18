6fd17a0040c3d3cf174f912bde1cc1fb615003bf
Alfergon <alvaro@blunder.es>
2018-01-18 19:33:12 +0100

    Use lifecycle methods

--- src/pages/journal/journal.ts
+++ src/pages/journal/journal.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {JournalEntry} from "../../model/journalEntry";
 import {EntryProvider} from "../../providers/entries/EntryProvider";
+import * as moment from "moment";
 
 @Component({
   selector: 'page-journal',
@@ -12,31 +13,30 @@ export class JournalPage {
 
   constructor(public navCtrl: NavController,
               private entryProvider: EntryProvider) {
-    this.entryProvider.entriesRef.valueChanges()
-      .subscribe(entries => {
-        console.log(entries);
+  }
 
-        entries.forEach(entry => {
-          let date = new Date(entry.date);
-          let month = date.getMonth() + 1;
-          let year = date.getFullYear();
-          let key = month + '-' + year;
+  ionViewDidLoad() {
+	  this.entryProvider.entriesRef.valueChanges()
+		  .subscribe(entries => {
+			  console.log(entries);
 
-          let i = this.groupedEntries.findIndex(g => g.key === key);
-          let grouped: GroupedEntries;
-          if (i === -1) {
-            grouped = new GroupedEntries();
-            grouped.key = key;
+			  entries.forEach(entry => {
+				  let key = moment(entry.date).format("MM/YYYY");
 
-            this.groupedEntries.push(grouped);
-          } else {
-            grouped = this.groupedEntries[i];
-          }
+				  let i = this.groupedEntries.findIndex(g => g.key === key);
+				  let grouped: GroupedEntries;
+				  if (i === -1) {
+					  grouped = new GroupedEntries();
+					  grouped.key = key;
 
-          grouped.entries.push(entry);
-        });
-      });
+					  this.groupedEntries.push(grouped);
+				  } else {
+					  grouped = this.groupedEntries[i];
+				  }
 
+				  grouped.entries.push(entry);
+			  });
+		  });
   }
 
   removeEntry() {
