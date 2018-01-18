98191fb0769229a549fc2b4c577b4306913a991c
Alfergon <alvaro@blunder.es>
2018-01-18 19:32:56 +0100

    Add signing in message

--- src/providers/users/CurrentUserProvider.ts
+++ src/providers/users/CurrentUserProvider.ts
@@ -3,6 +3,8 @@ import {AngularFireDatabase, AngularFireObject} from "angularfire2/database";
 import {Member} from "../../model/member";
 import {AngularFireAuth} from "angularfire2/auth";
 import {Subject} from "rxjs/Subject";
+import {LoadingController} from "ionic-angular";
+import {noop} from "rxjs/util/noop";
 
 @Injectable()
 export class CurrentUserProvider {
@@ -12,7 +14,13 @@ export class CurrentUserProvider {
   public currentUserObservable: Subject<Member> = new Subject<Member>();
 
   constructor(private db: AngularFireDatabase,
-              private fireAuth: AngularFireAuth) {
+              private fireAuth: AngularFireAuth,
+			  private loadingController: LoadingController) {
+	let loading = this.loadingController.create({
+		content: 'Signing in'
+	});
+
+	loading.present().then(noop);
 
     this.fireAuth.auth.signInWithEmailAndPassword("varomorf@gmail.com", "123456")
       .then(user => {
@@ -24,6 +32,8 @@ export class CurrentUserProvider {
             this.currentUser = user;
 
             this.currentUserObservable.next(user);
+
+            loading.dismiss().then(noop);
           });
       });
 
